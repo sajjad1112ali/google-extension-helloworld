4de4649716d8a17fe5a50d93d54aa0f5d9703efd
sajjad-ali <sajjad.ali@celeritas-solutions.com>
2019-12-24 20:22:51 +0500

    Added custom button

--- changeUpworkStyle/js/content.js
+++ changeUpworkStyle/js/content.js
@@ -7,4 +7,7 @@ $(function(){
       images[i].src = 'https://raw.githubusercontent.com/sajjad1112ali/google-extension-helloworld/master/changeUpworkStyle/images/Upwork-logo-black.png';
     }    
 
+    var anchor = '<a href="https://www.wikipedia.org/" class="btn btn-primary anchorCustomBtn" role="button" target="_blank" aria-pressed="true">Wiki Pedia</a>';
+    $("body").append(anchor);
 });
+
