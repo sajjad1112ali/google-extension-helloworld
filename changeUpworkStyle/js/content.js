7ad55b31271d4bc2ce73f26ea324f4c74a3a965e
sajjad-ali <sajjad.ali@celeritas-solutions.com>
2019-12-24 19:52:42 +0500

    New changes addressed

--- changeUpworkStyle/js/content.js
+++ changeUpworkStyle/js/content.js
@@ -1,8 +1,10 @@
 $(function(){
 
+  var customLogo = ' <img src="https://raw.githubusercontent.com/sajjad1112ali/google-extension-helloworld/master/changeUpworkStyle/images/Upwork-logo-black.png" class="img-fluid" style="width: 100%;">';
+  $("[data-cy='brand']").html(customLogo);
     var images = document.getElementsByTagName('img');
     for (var i = 0, l = images.length; i < l; i++) {
-      images[i].src = 'https://placekitten.com/' + images[i].width + '/' + images[i].height;
+      images[i].src = 'https://raw.githubusercontent.com/sajjad1112ali/google-extension-helloworld/master/changeUpworkStyle/images/Upwork-logo-black.png';
     }    
 
 });
