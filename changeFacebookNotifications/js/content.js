cfe4aa999e2c2b4a8910721d69edf290e72a0ff0
= <=>
2020-01-14 12:46:48 +0500

    Added color pickers for Messages, Notifications and Friend request

--- changeFacebookNotifications/js/content.js
+++ changeFacebookNotifications/js/content.js
@@ -1,13 +1,29 @@
 $(function(){
   chrome.runtime.sendMessage({todo: "showPageAction"});
 
-  chrome.storage.sync.get(["bg_color", "font_color"], function (styles) {
-      
+  chrome.storage.sync.get(["bg_color", "font_color", "m_bg_color", "m_font_color", "frequest_bg_color", "frequest_font_color"], function (styles) {
+     
+    // Notifications Color Pickers
     if (styles.bg_color) {
-        changeNotificationStyle("background-color", "#" + styles.bg_color);
+        changeNotificationStyle("background-color", "#" + styles.bg_color, "notificationsCountValue");
     }
     if (styles.font_color) {
-      changeNotificationStyle("color", "#" + styles.font_color);
+      changeNotificationStyle("color", "#" + styles.font_color, "notificationsCountValue");
+    }
+    // Setting messages Color Pickers
+    if (styles.m_bg_color) {
+      changeNotificationStyle("background-color", "#" + styles.m_bg_color, "mercurymessagesCountValue");
+    }
+    if (styles.m_font_color) {
+      changeNotificationStyle("color", "#" + styles.m_font_color, "mercurymessagesCountValue");
+    }
+    // Setting messages Color Pickers
+    if (styles.frequest_bg_color) {
+      changeNotificationStyle("background-color", "#" + styles.frequest_bg_color, "requestsCountValue");
+    }
+
+    if (styles.frequest_font_color) {
+      changeNotificationStyle("color", "#" + styles.frequest_font_color, "requestsCountValue");
     }
   });
 
@@ -15,28 +31,45 @@ $(function(){
   chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
     var addColor = "#" + request.clickedColor;
 
-    if (request.todo == "changeColor") {
-        changeNotificationStyle("background-color", addColor);
+    if (request.todo == "changeColor") { // Notification Bg
+        changeNotificationStyle("background-color", addColor, "notificationsCountValue");
         saveToChrom({"bg_color": request.clickedColor});
     }
     else
-    {
-      changeNotificationStyle("color", addColor);
+    if (request.todo == "changeFontColor") { // Notification font
+      changeNotificationStyle("color", addColor, "notificationsCountValue");
       saveToChrom({"font_color": request.clickedColor});
+    }
+    else
+    if (request.todo == "changeMsgBg") { // Messages font
+      changeNotificationStyle("background-color", addColor, "mercurymessagesCountValue");
+      saveToChrom({"m_bg_color": request.clickedColor});
+    }
+    else
+    if (request.todo == "changeMsgFont") { // Messages font
+      changeNotificationStyle("color", addColor, "mercurymessagesCountValue");
+      saveToChrom({"m_font_color": request.clickedColor});
+    }
+    else
+    if (request.todo == "changeFRequestBg") { // Friend request bg
+      changeNotificationStyle("background-color", addColor, "requestsCountValue");
+      saveToChrom({"frequest_bg_color": request.clickedColor});
+    }
+    else{
+      changeNotificationStyle("color", addColor, "requestsCountValue");
+      saveToChrom({"frequest_font_color": request.clickedColor});
     }
   });
 
 });
 
-function changeNotificationStyle(prop, color)
+function changeNotificationStyle(prop, color, targetDiv)
 {
-  $("._5ugg, ._3z_5").css(prop, color);
+  $(`#${targetDiv}`).css(prop, color);
 }
 function saveToChrom(ObjToSave){
-  // Saving roomIds to array
   chrome.storage.sync.set(ObjToSave, function(){
-    console.log("Room ID Saved to local storage");
-    console.log(ObjToSave);
+    
   });
 }
 
--- changeFacebookNotifications/js/popup.js
+++ changeFacebookNotifications/js/popup.js
@@ -1,30 +1,75 @@
-$(function(){
+$(function () {
 
 
-    chrome.storage.sync.get(["bg_color", "font_color"], function (styles) {
-      
+    chrome.storage.sync.get(["bg_color", "font_color", "m_bg_color", "m_font_color", "frequest_bg_color", "frequest_font_color"], function (styles) {
+
+
+        // Notifications Pickers
         if (styles.bg_color) {
             document.getElementById('colorPicker').jscolor.fromString(styles.bg_color);
         }
         if (styles.font_color) {
             document.getElementById('fontColorPicker').jscolor.fromString(styles.font_color);
         }
+
+        // Messages Pickers
+        if (styles.m_bg_color) {
+            document.getElementById('messagesBgColor').jscolor.fromString(styles.m_bg_color);
+        }
+        if (styles.m_font_color) {
+            document.getElementById('messagesFontColor').jscolor.fromString(styles.m_font_color);
+        }
+
+        // Messages Pickers
+        if (styles.frequest_bg_color) {
+            document.getElementById('friendRequestBgColor').jscolor.fromString(styles.frequest_bg_color);
+        }
+        if (styles.frequest_font_color) {
+            document.getElementById('friendRequestFontColor').jscolor.fromString(styles.frequest_font_color);
+        }
+
+
     });
 
-    $("#colorPicker").on("change paste keyup", function(){
+    // Notifications Pickers
+    $("#colorPicker").on("change paste keyup", function () {
         color = $(this).val();
-        var propertyObj = {todo: "changeColor", clickedColor: color};
+        var propertyObj = { todo: "changeColor", clickedColor: color };
         changeQuestionColor(propertyObj);
     });
 
-    $("#fontColorPicker").on("change paste keyup", function(){
+    $("#fontColorPicker").on("change paste keyup", function () {
         color = $(this).val();
-        var propertyObj = {todo: "changeFontColor", clickedColor: color};
+        var propertyObj = { todo: "changeFontColor", clickedColor: color };
         changeQuestionColor(propertyObj);
     });
 
-    function changeQuestionColor(toDo){
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    // Messages Pickers
+    $("#messagesBgColor").on("change paste keyup", function () {
+        color = $(this).val();
+        var propertyObj = { todo: "changeMsgBg", clickedColor: color };
+        changeQuestionColor(propertyObj);
+    });
+    $("#messagesFontColor").on("change paste keyup", function () {
+        color = $(this).val();
+        var propertyObj = { todo: "changeMsgFont", clickedColor: color };
+        changeQuestionColor(propertyObj);
+    });
+
+    // Friend request Pickers
+    $("#friendRequestBgColor").on("change paste keyup", function () {
+        color = $(this).val();
+        var propertyObj = { todo: "changeFRequestBg", clickedColor: color };
+        changeQuestionColor(propertyObj);
+    });
+    $("#friendRequestFontColor").on("change paste keyup", function () {
+        color = $(this).val();
+        var propertyObj = { todo: "changeFRequestFont", clickedColor: color };
+        changeQuestionColor(propertyObj);
+    });
+
+    function changeQuestionColor(toDo) {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, toDo);
         });
     }
